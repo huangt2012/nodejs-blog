2231a98b67b30e13d5a5fc549a512e378f5b3822
tinnyhuang <tinnyhuang@tencent.com>
2023-02-26 17:58:21 +0800

    feat: readline 分析日志

---
+++ blog-1/src/utils/readline.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+
+const fileName = path.join(__dirname, '../', '../', 'logs', 'access.log');
+
+const readStream = fs.createReadStream(fileName);
+
+const rl = readline.createInterface({
+  input: readStream
+})
+
+let chromeNum = 0;
+let sum = 0;
+
+// 监听读取一行
+rl.on('line', (lineData) => {
+  if (!lineData) {
+    return;
+  }
+
+  sum += 1;
+
+  const arr = lineData.split('--');
+  if (arr[2] && arr[2].indexOf('Chrome') > 0) {
+    chromeNum += 1;
+  }
+})
+
+// 监听读取结束
+rl.on('close', () => {
+  console.log('chrome 占比', chromeNum / sum)
+})
