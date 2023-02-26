e198e8e3731a98a4d6ef633c1b924f6683acda61
tinnyhuang <tinnyhuang@tencent.com>
2023-02-26 18:07:05 +0800

    feat: mysql escape 防止 sql 注入

--- blog-1/src/controller/user.js
+++ blog-1/src/controller/user.js
@@ -1,7 +1,7 @@
-const { exec } = require("../db/mysql");
+const { exec, escape } = require("../db/mysql");
 
 const login = (username, password) => {
-  const sql = `select username, realname from users where username='${username}' and password=${password}`;
+  const sql = `select username, realname from users where username=${escape(username)} and password=${escape(password)}`;
 
   return exec(sql).then((rows) => rows[0])
 }
