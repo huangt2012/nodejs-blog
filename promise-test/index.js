489919fb04a1cae47257538af2e439909da509b6
tinnyhuang <tinnyhuang@tencent.com>
2023-02-28 23:17:21 +0800

    feat: async/await

--- promise-test/index.js
+++ promise-test/index.js
@@ -18,15 +18,31 @@ function getFileContent(fileName) {
   })
 }
 
-getFileContent('a.json').then((aData) => {
-  console.log(aData);
-  return getFileContent(aData.next)
-}).then((bData) => {
-  console.log(bData)
+// getFileContent('a.json').then((aData) => {
+//   console.log(aData);
+//   return getFileContent(aData.next)
+// }).then((bData) => {
+//   console.log(bData)
 
-  return getFileContent(bData.next)
-}).then(cData => {
+//   return getFileContent(bData.next)
+// }).then(cData => {
   
-    console.log(cData)
+//     console.log(cData)
   
-}) 
+// }) 
+
+
+// async await 要点
+// 1. await 后面可以追加 promise 对象,获取 resolv 的值
+// 2. await 必须包裹在 async 中
+// 3. async 函数执行后返回的也是 promise 对象
+// 4. try-catch 可以捕获 reject 的值
+
+async function readFiles() {
+  const aData = await getFileContent('a.json');
+  console.log(aData);
+  const bData = await getFileContent(aData.next);
+  console.log(bData)
+}
+
+readFiles()
